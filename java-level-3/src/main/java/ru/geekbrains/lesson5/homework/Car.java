da191c71e406ddf32805d1f8c7283e1adc5b0fd5
VicN <vnikolaeff989@gmail.com>
2021-07-13 14:25:26 +0300

    HomeWork5
    Использование Phaser

--- java-level-3/src/main/java/ru/geekbrains/lesson5/homework/Car.java
+++ java-level-3/src/main/java/ru/geekbrains/lesson5/homework/Car.java
@@ -2,6 +2,7 @@ package ru.geekbrains.lesson5.homework;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Phaser;
 
 public class Car implements Runnable {
 
@@ -12,20 +13,23 @@ public class Car implements Runnable {
     }
 
     private Race race;
-    private CountDownLatch latch1;
-    private CountDownLatch latch2;
-    private CyclicBarrier barrier;
+//    private CountDownLatch latch1;
+//    private CountDownLatch latch2;
+//    private CyclicBarrier barrier;
+    private Phaser phaser;
 
     private int speed;
     private String name;
     private static boolean winner;
 
-    public Car(Race race, int speed, CountDownLatch latch1, CyclicBarrier barrier, CountDownLatch latch2) {
+    public Car(Race race, int speed, Phaser phaser) {
         this.race = race;
         this.speed = speed;
-        this.latch1 = latch1;
-        this.latch2 = latch2;
-        this.barrier = barrier;
+//        this.latch1 = latch1;
+//        this.latch2 = latch2;
+//        this.barrier = barrier;
+        this.phaser = phaser;
+        this.phaser.register();
 
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
@@ -41,12 +45,14 @@ public class Car implements Runnable {
 
     @Override
     public void run() {
+//        phaser.register();
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
-            latch1.countDown();
-            barrier.await();
+//            latch1.countDown();
+//            barrier.await();
+            phaser.arriveAndAwaitAdvance();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,6 +63,7 @@ public class Car implements Runnable {
             winner = true;
             System.out.println(this.name + " - WIN");
         }
-        latch2.countDown();
+//        latch2.countDown();
+        phaser.arriveAndAwaitAdvance();
     }
 }
--- java-level-3/src/main/java/ru/geekbrains/lesson5/homework/MainClass.java
+++ java-level-3/src/main/java/ru/geekbrains/lesson5/homework/MainClass.java
@@ -13,33 +13,38 @@ public class MainClass {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 
         Semaphore semaphore = new Semaphore(CARS_COUNT / 2);
-        CountDownLatch latch1 = new CountDownLatch(CARS_COUNT);
-        CountDownLatch latch2 = new CountDownLatch(CARS_COUNT);
-        CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT);
+//        CountDownLatch latch1 = new CountDownLatch(CARS_COUNT);
+//        CountDownLatch latch2 = new CountDownLatch(CARS_COUNT);
+//        CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT);
+        Phaser phaser = new Phaser();
+        phaser.register();
 
         Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
 
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), latch1, barrier, latch2);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), phaser);
         }
 
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
 
-        try {
-            latch1.await();
+//        try {
+//            latch1.await();
+            phaser.arriveAndAwaitAdvance();
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            latch2.await();
+//        try {
+//            latch2.await();
+            phaser.arriveAndAwaitAdvance();
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            phaser.arriveAndDeregister();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
