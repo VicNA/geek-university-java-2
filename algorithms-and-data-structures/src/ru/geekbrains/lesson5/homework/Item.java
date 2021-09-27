692cf58d59fc63f557b48401b3abbe63994f0df1
VicN <vnikolaeff989@gmail.com>
2021-09-27 13:56:19 +0300

    HomeWork5. Update

---
+++ algorithms-and-data-structures/src/ru/geekbrains/lesson5/homework/Item.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.lesson5.homework;
+
+public class Item {
+
+    private int price;
+    private int weight;
+
+    public Item(int price, int weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
